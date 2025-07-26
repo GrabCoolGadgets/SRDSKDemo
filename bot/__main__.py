e35ed7a8e0535d2244d6752795c0e0028058f389
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-07-26 08:32:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,3 +2,20 @@ from bot import Bot
 
 if __name__ == '__main__':
     Bot().run()
+
+    # Dummy Flask server for Render
+    import os
+    from flask import Flask
+    import threading
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return "Bot is running fine."
+
+    def run():
+        port = int(os.environ.get("PORT", 8080))
+        app.run(host="0.0.0.0", port=port)
+
+    threading.Thread(target=run).start()
